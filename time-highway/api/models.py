0fedb652ffac660355f607414b5ea9cb1d9c9f8e
vahid <vahid@kharazi.net>
2014-09-02 23:23:04 +0430

    Primitive implemention of data models

--- time-highway/api/models.py
+++ time-highway/api/models.py
@@ -1,5 +1,6 @@
 from mongoengine import Document, StringField,\
-    DateTimeField, EmailField
+    DateTimeField, EmailField, ListField, ReferenceField, BooleanField\
+    EmbeddedDocumentField, FloatField, IntField
 import datetime
 
 
@@ -9,6 +10,7 @@ class UserModel(Document):
     email = EmailField(max_length=100, required=True, unique=True)
     password = StringField(min_length=1, max_length=200, required=True)
     signup_date = DateTimeField(default=datetime.datetime.utcnow)
+    branch = ReferenceField('BranchModel') # Root branch
 
     def is_active(self):
         return True
@@ -21,3 +23,57 @@ class UserModel(Document):
 
     def get_id(self):
         return unicode(self.id)
+
+
+class GroupModel(Document):
+    owner = ReferenceField(UserModel, required=True)
+    members = ListField(ReferenceField(UserModel))
+
+
+class PermissionModel(EmbeddedDocument):
+    group = ReferenceField(GroupModel, required=True)
+    read_access = BooleanField(required=True, default=True)
+    write_access = BooleanField(required=True, default=True)
+    edit = BooleanField(required=True, default=False)
+    delete = BooleanField(required=True, default=False)
+
+
+class TModel(EmbeddedDocument):
+    value = StringField()
+    type = StringField() 
+    granularity = IntField(default=1000)
+    probability = FloatField(min_value=0, max_value=1)
+
+
+class DTModel(EmbeddedDocument):
+    value = StringField()
+
+
+class EventDataModel(EmbeddedDocument):
+    what = StringField()
+
+
+class EventModel(Document):
+    branch = ReferenceField('BranchModel')
+    source = DateTimeField(default=datetime.datetime.utcnow)
+    data = EmbeddedDocumentField(EventDataModel)
+    t = EmbeddedDocument(TModel)
+    dt = EmbeddedDocument(DTModel)
+    tags = ListField(StringField(max_length=50))
+    types = ListField(StringField(max_length=50))
+    
+    # By defualt must inherit from BranchModel.permissions
+    permissions = ListField(
+        EmbeddedDocumentField(PermissionModel)
+    ) 
+
+
+class BranchModel(Document):
+    name = StringField(max_length=100, required=True)
+    description = StringField()
+    # owner = ReferenceField(UserModel, required=True)
+    sub_branchs = ListField(ReferenceField('BranchModel'))
+    events = ListField(ReferenceField(EventModel))
+    permissions = ListField(
+        EmbeddedDocumentField(PermissionModel)
+    )
