794d92a70c544a7f1c98550c6523d7604d2fed63
vahid <vahid@kharazi.net>
2014-09-23 00:22:51 +0330

    working on POST event, we have trouble with nested

--- time-highway/api/validation.py
+++ time-highway/api/validation.py
@@ -21,7 +21,7 @@ auth_parser.add_argument('email', type=email_type, required=True)
 auth_parser.add_argument('password', type=unicode, required=True)
 
 group_parser = reqparse.RequestParser()
-group_parser.add_argument('members', type=list, required=True)
+# group_parser.add_argument('members', type=list, required=True)
 
 user_parser = reqparse.RequestParser()
 
@@ -29,4 +29,10 @@ get_event_parser = reqparse.RequestParser()
 get_event_parser.add_argument('event_id', type=str)
 
 add_event_parser = reqparse.RequestParser()
-add_event_parser.add_argument()
+add_event_parser.add_argument('parent', type=str, required=False)
+add_event_parser.add_argument('source', type=str)
+add_event_parser.add_argument('data', type=dict)
+add_event_parser.add_argument('t', type=dict)
+add_event_parser.add_argument('dt', type=dict)
+add_event_parser.add_argument('types', type=str, action='append')
+add_event_parser.add_argument('tags', type=str, action='append')
