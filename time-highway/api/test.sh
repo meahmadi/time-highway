7f6fcc83b33b6e121379844cb328273efe0acf41
vahid <vahid@kharazi.net>
2014-10-03 21:40:57 +0330

    Start test APIs

---
+++ time-highway/api/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#OPTIONS="-H \"Content-Type: application/json\" -i -b cook -c cook"
+OPTIONS="-i -b cook -c cook"
+
+if [[ -z "$2" ]]; then
+  BASEURL=http://api.timehighway.ir
+else
+  BASEURL=$2
+fi
+
+case "$1" in
+  test)
+    curl $OPTIONS -X GET $BASEURL/test
+    ;;
+  signup)
+    curl -H "Content-Type: application/json" $OPTIONS -X POST -d '{"email": "example@gmail.com", "password": "testtest"}' $BASEURL/users/signup
+    ;;
+  login)
+    curl -H "Content-Type: application/json" $OPTIONS -X POST -d '{"email": "example@gmail.com", "password": "testtest"}' $BASEURL/users/login
+    ;;
+  logout)
+    curl -H "Content-Type: application/json" $OPTIONS -X POST $BASEURL/users/logout
+    ;;
+esac
